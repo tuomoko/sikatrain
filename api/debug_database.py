79ef0985a1f024508cf62ef98f4d5ec022dd18b8
Tuomo Kohtamäki <tuomo.kohtamaki@gmail.com>
2018-02-08 22:14:27 +0200

    Adding command to throw all database contents out for debugging

--- api/debug_database.py
+++ api/debug_database.py
@@ -6,5 +6,12 @@ client = MongoClient("mongodb://localhost:27017")
 db = client.sikatables
 collection = db.train_data
 
+print "Printing all training data"
+for record in collection.find():
+	pprint.pprint(record)
+
+collection = db.score_data
+
+print "Printing all score data"
 for record in collection.find():
 	pprint.pprint(record)
